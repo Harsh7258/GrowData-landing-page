8fc5522568673d1cd92011cde949afd78d435253
K Harsh <harshnaidu2u@gmail.com>
2023-10-13 19:36:56 +0530

    final changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,11 +11,11 @@ const Navbar = () => {
         <div className='text flex justify-between items-center h-20 max-w-[1240px] mx-auto px-4'>
             <h3 className='w-full text-2xl font-bold text2'>GrowData.</h3>
             <ul className='hidden md:flex justify-between'>
-                <li className='p-4'>Home</li>
-                <li className='p-4'>Company</li>
-                <li className='p-4'>Resources</li>
-                <li className='p-4'>About</li>
-                <li className='p-4'> Contacts</li>
+                <li className='p-4'><a href='/'> Home </a></li>
+                <li className='p-4'><a href='/'> Company </a></li>
+                <li className='p-4'><a href='/'> Resources </a></li>
+                <li className='p-4'><a href='/'> About </a></li>
+                <li className='p-4'><a href='/'> Contacts </a></li>
             </ul>
 
             <div
@@ -31,11 +31,11 @@ const Navbar = () => {
                 'navToggle bg-color'}>
                  <h3 className='w-full text-2xl font-bold text2 m-6'>GrowData.</h3>
                 <ul className='uppercase pt-20'>
-                    <li className='p-4 border-b border-gray-600'>Home</li>
-                    <li className='p-4 border-b border-gray-600'>Company</li>
-                    <li className='p-4 border-b border-gray-600'>Resources</li>
-                    <li className='p-4 border-b border-gray-600'>About</li>
-                    <li className='p-4 border-b border-gray-600'> Contacts</li>
+                    <li className='p-4 border-b border-gray-600'><a href='/'> Home </a></li>
+                    <li className='p-4 border-b border-gray-600'><a href='/'> Company </a></li>
+                    <li className='p-4 border-b border-gray-600'><a href='/'> Resources </a></li>
+                    <li className='p-4 border-b border-gray-600'><a href='/'> About </a></li>
+                    <li className='p-4 border-b border-gray-600'><a href='/'> Contacts </a></li>
                 </ul>
             </div>
         </div>
